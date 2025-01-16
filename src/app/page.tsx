e113ff57a7fe1e126204b3ad439978a643e5c2b3
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2025-01-16 16:02:49 +0800

    Fixed password validation & added animations.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -5,6 +6,7 @@ export default function Home() {
       <h1 className="font-sans font-bold tracking-tighter"> perx </h1>
       <p className="font-mono">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque ipsa cum nostrum harum consectetur cupiditate. Cupiditate suscipit atque minus. Minus adipisci, expedita aliquid ad doloribus tempore optio nulla reprehenderit saepe!
       Ab sequi nulla excepturi consectetur minima ipsum eius officia minus veritatis, deserunt aspernatur nisi quisquam laborum reprehenderit numquam, qui earum, tempora vel illo aliquid perferendis repudiandae commodi fugit nam. Amet?</p>
+      <Link href="/merchant/register">Go to merchant</Link>
     </div>
   )
 }

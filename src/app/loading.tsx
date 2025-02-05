061fc2f1e2394b8117938c1eb9a89f194cfb3d07
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2025-02-05 13:04:34 +0800

    Implemented merchant auth

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,11 +2,13 @@ import { LoaderCircle } from 'lucide-react';
 
 export default function Loading() {
   return (
-    <LoaderCircle
-      className="text-perx-red -ms-1 me-2 animate-spin"
-      size={16}
-      strokeWidth={2}
-      aria-hidden="true"
-    />
+    <div className="flex h-dvh w-dvw items-center justify-center text-red-900">
+      <LoaderCircle
+        className="text-perx-red -ms-1 me-2 animate-spin"
+        size={40}
+        strokeWidth={2}
+        aria-hidden="true"
+      />
+    </div>
   );
 }

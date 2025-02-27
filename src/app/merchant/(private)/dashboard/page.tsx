110f3512f18f7242c24bcbde99e35905e778ced5
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2025-02-27 13:38:26 +0800

    Added padding in dashboard cards

--- src/app/merchant/(private)/dashboard/page.tsx
+++ src/app/merchant/(private)/dashboard/page.tsx
@@ -1,21 +1,8 @@
-import { logoutMerchant } from '@/actions/merchant/auth';
-import { Button } from '@/components/ui/button';
 import { createClient } from '@/utils/supabase/server';
-// import { redirect } from 'next/navigation';
 import UserCard from '@/components/merchant/Usercard';
-import {
-  ArrowRight,
-  Tickets,
-  ShoppingCart,
-  UsersRound,
-  PhilippinePeso,
-  EllipsisVertical,
-} from 'lucide-react';
-import { profile } from 'console';
+import { ArrowRight, Tickets, UsersRound, PhilippinePeso } from 'lucide-react';
 import LineChart from '@/components/merchant/lineChart';
 import TopCouponsMonth from '@/components/merchant/TopCouponsMonth';
-import Link from 'next/link';
-// import EventCalendar from '@/components/merchant/EventCalendar';
 
 export default async function DashboardPage() {
   const supabase = await createClient();
@@ -36,52 +23,51 @@ export default async function DashboardPage() {
   // );
 
   return (
-      <div className='flex gap-4 flex-col md:flex-row'>
-        {/* LEFT */}
-        <div className='w-full lg:w-2/3 flex flex-col gap-4'>
-          {/* USER CARDS */}
-          <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-            <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
-              <UserCard 
-                title="REVENUE THIS MONTH" 
-                amount="5,365" 
-                icon={ArrowRight}
-                amtIcon={PhilippinePeso}
-                titleClassName="mx-4 my-2 text-[18px] font-mono text-perx-white"
-                amtClassName="text-[85px] font-mono text-perx-white"
-                amtIconSize={90}
-                bgColor='bg-perx-canopy/85'
-                color='white'
-              />
-            </div>
-            <div className="flex lg:flex-col gap-4">
-              <UserCard 
-              title="COUPONS SOLD" 
-              amount="37" 
+    <div className="flex flex-col gap-4 md:flex-row">
+      {/* LEFT */}
+      <div className="flex w-full flex-col gap-4 lg:w-2/3">
+        {/* USER CARDS */}
+        <div className="grid grid-cols-1 gap-4 lg:grid-cols-2">
+          <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
+            <UserCard
+              title="REVENUE THIS MONTH"
+              amount="5,365"
+              icon={ArrowRight}
+              amtIcon={PhilippinePeso}
+              titleClassName="mx-4 my-2 text-[18px] font-mono text-perx-white"
+              amtClassName="text-6xl font-mono text-perx-white"
+              amtIconSize={60}
+              bgColor="bg-perx-canopy"
+              color="white"
+              textColor=""
+            />
+          </div>
+          <div className="flex gap-4 lg:flex-col">
+            <UserCard
+              title="COUPONS SOLD"
+              amount="37"
               amtIcon={Tickets}
               titleClassName="my-2 mx-3 text-[15px] font-mono text-perx-white"
               amtClassName="my-2 text-5xl font-mono text-perx-white"
               amtIconSize={30}
-              bgColor='bg-perx-crimson/85'
-              color='white'
-
-              />
-              <UserCard 
-              title="CUSTOMERS" 
-              amount="32" 
+              bgColor="bg-perx-crimson/85"
+              color="white"
+            />
+            <UserCard
+              title="CUSTOMERS"
+              amount="32"
               amtIcon={UsersRound}
               titleClassName="my-2 mx-3 text-[15px] font-mono text-perx-white"
               amtClassName="my-2 text-5xl font-mono text-perx-white"
               amtIconSize={30}
-              bgColor='bg-perx-navy/85'
-              color='white'
-
-              />
-            </div>
+              bgColor="bg-perx-navy/85"
+              color="white"
+            />
           </div>
-          {/* LINE CHART */}
-        <div className="w-full h-[450px]">
-          <LineChart/>
+        </div>
+        {/* LINE CHART */}
+        <div className="h-[450px] w-full">
+          <LineChart />
         </div>
       </div>
       {/* RIGHT */}
@@ -90,7 +76,4 @@ export default async function DashboardPage() {
       </div>
     </div>
   );
-
-
-
 }
--- src/components/merchant/Usercard.tsx
+++ src/components/merchant/Usercard.tsx
@@ -11,6 +11,7 @@ interface UserCardProps {
   amtIconSize?: number;
   bgColor?: string;
   color?: string;
+  textColor?: string;
 }
 
 const UserCard = ({
@@ -20,21 +21,29 @@ const UserCard = ({
   amtIcon: AmtIcon,
   titleClassName,
   amtClassName,
-  amtIconSize=20,
+  amtIconSize = 20,
   bgColor,
   color,
-  
+  textColor,
 }: UserCardProps) => {
   return (
-    <div className={`${bgColor} min-w-[130px] flex-1 rounded-2xl p-4}`}>
+    <div className={`${bgColor} min-w-[130px] flex-1 rounded-2xl p-4`}>
       <div className="flex items-center justify-between">
-        <span className={`${titleClassName}`}>{title}</span>
+        <span className={`${titleClassName} ${textColor}`}>{title}</span>
         <Link href="/merchant/monthly-records">
-        {Icon && <Icon size={30} style={{color}} className="mr-2 hover:bg-perx-canopy rounded-full p-1"/>}
+          {Icon && (
+            <Icon
+              size={30}
+              style={{ color }}
+              className="hover:bg-perx-canopy mr-2 rounded-full p-1"
+            />
+          )}
         </Link>
       </div>
       <div className="flex items-center">
-        {AmtIcon && <AmtIcon size={amtIconSize} style={{color}} className="mx-3" />}
+        {AmtIcon && (
+          <AmtIcon size={amtIconSize} style={{ color }} className="mx-3" />
+        )}
         <h1 className={`${amtClassName}`}>{amount}</h1>
       </div>
     </div>

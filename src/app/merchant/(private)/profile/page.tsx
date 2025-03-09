ca5315446d3cff1c13e7ae205a0282d654a4a5f3
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2025-03-09 16:09:01 +0800

    Started working on merchant profile

--- src/app/merchant/(private)/profile/page.tsx
+++ src/app/merchant/(private)/profile/page.tsx
@@ -1,8 +1,37 @@
 import { getMerchantProfile } from '@/actions/merchant/profile';
 import PerxTabs from '@/components/custom/PerxTabs';
+import { Button } from '@/components/ui/button';
 import type { MerchantProfile } from '@/lib/merchant/profileSchema';
 import { createClient } from '@/utils/supabase/server';
+import {
+  ArchiveIcon,
+  SettingsIcon,
+  SquareLibraryIcon,
+  TicketsIcon,
+} from 'lucide-react';
+import Link from 'next/link';
 import { redirect } from 'next/navigation';
+import { JSX } from 'react';
+
+interface ProfileNavItems {
+  icon: JSX.Element;
+  name: string;
+}
+
+const profileNavItems: ProfileNavItems[] = [
+  {
+    name: 'Coupons',
+    icon: <TicketsIcon size={16} aria-hidden="true" />,
+  },
+  {
+    name: 'Collections',
+    icon: <SquareLibraryIcon size={16} aria-hidden="true" />,
+  },
+  {
+    name: 'Archive',
+    icon: <ArchiveIcon size={16} aria-hidden="true" />,
+  },
+];
 
 export default async function MerchantProfile() {
   const supabase = await createClient();
@@ -17,27 +46,53 @@ export default async function MerchantProfile() {
   const data = await getMerchantProfile(user.id);
 
   return (
-    <section>
+    <section className="flex flex-col gap-8 md:px-20">
       <ProfileInfo data={data} />
-      <PerxTabs />
+      <PerxTabs tabItems={profileNavItems} />
     </section>
   );
 }
 
 function ProfileInfo({ data }: { data: MerchantProfile }) {
   return (
-    <section>
-      <div>
+    <section className="flex flex-col gap-4 md:flex-row md:gap-8">
+      <div className="flex basis-1/3 items-center justify-center">
         <img
           src={data.logo}
           alt="Merchant Logo"
-          className="aspect-square h-48 w-48 rounded-full object-cover"
+          className="aspect-square size-32 rounded-full object-cover md:size-48"
         />
-        <h3 className="text-3xl font-bold">{data.name}</h3>
-        <p>{data.bio}</p>
-        <p>{data.address}</p>
-        <p>{data.email}</p>
+      </div>
+      <div className="flex grow flex-col items-center justify-center gap-4 md:items-start">
+        <div className="flex items-center gap-8">
+          <h3 className="text-xl font-bold md:text-3xl">{data.name}</h3>
+          <div className="hidden md:flex">
+            <ButtonGroup />
+          </div>
+        </div>
+        <div className="md:hidden">
+          <ButtonGroup />
+        </div>
+        <div>
+          <p className="text-sm">{data.bio}</p>
+          <p className="text-sm">{data.address}</p>
+          <p className="text-sm">{data.email}</p>
+        </div>
       </div>
     </section>
+  );
+}
+
+function ButtonGroup() {
+  return (
+    <div className="flex items-center justify-center gap-2 md:gap-4">
+      <Button>Edit profile</Button>
+      <Button variant={'secondary'}>Share profile</Button>
+      <Link href="/merchant/settings">
+        <Button variant={'secondary'}>
+          <SettingsIcon />
+        </Button>
+      </Link>
+    </div>
   );
 }
--- src/components/custom/PerxTabs.tsx
+++ src/components/custom/PerxTabs.tsx
@@ -1,93 +1,29 @@
-import { Badge } from '@/components/ui/badge';
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
-import {
-  BoxIcon,
-  ChartLine,
-  HouseIcon,
-  PanelsTopLeftIcon,
-  SettingsIcon,
-  UsersRoundIcon,
-} from 'lucide-react';
+import { JSX } from 'react';
 
-export default function PerxTabs() {
+interface TabItems {
+  icon: JSX.Element;
+  name: string;
+}
+
+export default function PerxTabs({ tabItems }: { tabItems: TabItems[] }) {
   return (
     <Tabs defaultValue="tab-1">
       <ScrollArea>
-        <TabsList className="text-foreground mb-3 h-auto gap-2 rounded-none border-b bg-transparent px-0 py-1">
-          <TabsTrigger
-            value="tab-1"
-            className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
-          >
-            <HouseIcon
-              className="-ms-0.5 me-1.5 opacity-60"
-              size={16}
-              aria-hidden="true"
-            />
-            Overview
-          </TabsTrigger>
-          <TabsTrigger
-            value="tab-2"
-            className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
-          >
-            <PanelsTopLeftIcon
-              className="-ms-0.5 me-1.5 opacity-60"
-              size={16}
-              aria-hidden="true"
-            />
-            Projects
-            <Badge
-              className="bg-primary/15 ms-1.5 min-w-5 px-1"
-              variant="secondary"
-            >
-              3
-            </Badge>
-          </TabsTrigger>
-          <TabsTrigger
-            value="tab-3"
-            className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
-          >
-            <BoxIcon
-              className="-ms-0.5 me-1.5 opacity-60"
-              size={16}
-              aria-hidden="true"
-            />
-            Packages
-            <Badge className="ms-1.5">New</Badge>
-          </TabsTrigger>
-          <TabsTrigger
-            value="tab-4"
-            className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
-          >
-            <UsersRoundIcon
-              className="-ms-0.5 me-1.5 opacity-60"
-              size={16}
-              aria-hidden="true"
-            />
-            Team
-          </TabsTrigger>
-          <TabsTrigger
-            value="tab-5"
-            className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
-          >
-            <ChartLine
-              className="-ms-0.5 me-1.5 opacity-60"
-              size={16}
-              aria-hidden="true"
-            />
-            Insights
-          </TabsTrigger>
-          <TabsTrigger
-            value="tab-6"
-            className="hover:bg-accent hover:text-foreground data-[state=active]:after:bg-primary data-[state=active]:hover:bg-accent relative after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.5 data-[state=active]:bg-transparent data-[state=active]:shadow-none"
-          >
-            <SettingsIcon
-              className="-ms-0.5 me-1.5 opacity-60"
-              size={16}
-              aria-hidden="true"
-            />
-            Settings
-          </TabsTrigger>
+        <TabsList className="text-foreground mb-3 h-auto w-full gap-8 rounded-none border-b bg-transparent px-0 py-1">
+          {tabItems.map((item, index) => {
+            return (
+              <TabsTrigger
+                value={`tab-${index + 1}`}
+                key={index}
+                className="hover:bg-perx-crimson/10 data-[state=active]:hover:bg-perx-crimson/10 hover:text-foreground data-[state=active]:after:bg-perx-crimson data-[state=active]:text-perx-crimson relative flex cursor-pointer items-center justify-center gap-2 px-8 after:absolute after:inset-x-0 after:bottom-0 after:-mb-1 after:h-0.75 after:rounded-full data-[state=active]:bg-transparent data-[state=active]:shadow-none"
+              >
+                {item.icon}
+                <span className="hidden sm:block">{item.name}</span>
+              </TabsTrigger>
+            );
+          })}
         </TabsList>
         <ScrollBar orientation="horizontal" />
       </ScrollArea>

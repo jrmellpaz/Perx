830cd94db5c76bc1063ea999d41b1d3da3ba2f5e
kapadilla <kapadilla@up.edu.ph>
2025-04-17 22:44:39 +0800

    [Style][Kim]  Adjusted the authentications

--- src/app/(consumer)/(auth)/login/page.tsx
+++ src/app/(consumer)/(auth)/login/page.tsx
@@ -1,15 +1,13 @@
 import { ConsumerLogo } from '@/components/consumer/ConsumerLogo';
 import ConsumerLoginForm from '@/components/consumer/ConsumerLogin';
-import { createClient } from '@/utils/supabase/server';
-import { redirect } from 'next/navigation';
 
 export default async function ConsumerLoginPage() {
   return (
-    <section className="flex h-full w-9/10 flex-col gap-4 overflow-hidden py-8 sm:w-4/5">
-      <div className="flex h-12 w-fit">
-        <ConsumerLogo logoClass="text-3xl pb-2" />
+    <div className="flex h-full w-9/10 flex-col gap-4 sm:w-4/5">
+      <div className="flex h-12 w-fit items-center">
+        <ConsumerLogo logoClass="text-3xl pb-[5px]" />
       </div>
       <ConsumerLoginForm />
-    </section>
+    </div>
   );
 }

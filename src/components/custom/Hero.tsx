2257aeded35c092fc03b4c3a339bb69f4ca099be
Anne <anneeloisaabelido14@gmail.com>
2025-04-26 09:08:00 +0800

    Lpage register

--- src/components/custom/Hero.tsx
+++ src/components/custom/Hero.tsx
@@ -9,18 +9,19 @@ export default function Hero() {
       <div className="mb-40 flex flex-col items-center space-y-8">
         <img src="/perxIcon.png" className="mb-8 h-85 w-85" alt="Perx Logo" />
         {/* Sign in merchant/consumer */}
+        <p className="font-mono">Register as</p>
         <div className="flex space-x-4">
           <Link
             href="/merchant/register"
             className="bg-perx-crimson text-perx-white hover:bg-perx-cloud hover:text-perx-crimson hover:border-perx-crimson border-perx-crimson rounded-md border-2 px-6 py-2 font-mono duration-200"
           >
-            Sign up as Merchant
+            Merchant
           </Link>
           <Link
             href="/register"
             className="border-perx-crimson text-perx-crimson hover:bg-perx-cloud hover:text-perx-white rounded-md border-2 px-6 py-2 font-mono duration-200"
           >
-            Sign up as Consumer
+            Consumer
           </Link>
         </div>
       </div>

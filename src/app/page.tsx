e8ae66dce29e70200ed899d682ff12c16c7d9206
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2025-04-29 20:10:45 +0800

    before infinite checkout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,32 +2,29 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="overflow-x-hidden">
-      <div className="flex min-h-screen w-full flex-col items-center justify-center bg-[url('/bgLPF.png')] bg-cover bg-center">
-        {/* Logo */}
-        <div className="flex flex-col items-center space-y-8">
-          <img src="/perxIcon.png" className="h-85 w-85" alt="Perx Logo" />
-          {/* Sign in merchant/consumer */}
-          <div className="flex items-center gap-2 font-sans">
-            <p className="text-perx-black text-5xl font-bold tracking-tighter">
-              perx
-            </p>
-          </div>
-          {/* <p className="font-mono">Log in as</p> */}
-          <div className="flex space-x-4">
-            <Link
-              href="/merchant/login"
-              className="bg-perx-crimson text-perx-white hover:bg-perx-crimson/30 hover:text-perx-crimson hover:border-perx-crimson border-perx-crimson rounded-md border-2 px-6 py-2 font-mono duration-200"
-            >
-              Merchant
-            </Link>
-            <Link
-              href="/explore"
-              className="border-perx-crimson text-perx-crimson hover:bg-perx-crimson/30 hover:text-perx-crimson rounded-md border-2 px-6 py-2 font-mono duration-200"
-            >
-              Consumer
-            </Link>
-          </div>
+    <div className="flex h-dvh w-dvw flex-col items-center justify-center bg-[url('/bgLPF.png')] bg-cover bg-center">
+      {/* Logo */}
+      <div className="flex flex-col items-center space-y-8">
+        <img src="/perxIcon.png" className="h-85 w-85" alt="Perx Logo" />
+        {/* Sign in merchant/consumer */}
+        <div className="flex items-center gap-2 font-sans">
+          <p className="text-perx-black text-5xl font-black tracking-[-0.075em]">
+            perx
+          </p>
+        </div>
+        <div className="flex space-x-4">
+          <Link
+            href="/merchant/login"
+            className="bg-perx-crimson text-perx-white hover:bg-perx-crimson/30 hover:text-perx-crimson hover:border-perx-crimson border-perx-crimson rounded-md border-2 px-6 py-2 font-mono duration-200"
+          >
+            Merchant
+          </Link>
+          <Link
+            href="/explore"
+            className="border-perx-crimson text-perx-crimson hover:bg-perx-crimson/30 hover:text-perx-crimson rounded-md border-2 px-6 py-2 font-mono duration-200"
+          >
+            Consumer
+          </Link>
         </div>
       </div>
     </div>
--- src/components/custom/PerxInfiniteScroll.tsx
+++ src/components/custom/PerxInfiniteScroll.tsx
@@ -9,14 +9,16 @@ export function LoadMore() {
 
   useEffect(() => {
     if (inView) {
-      // alert('Load more items!');
+      alert('Load more items!');
     }
   }, [inView]);
 
   return (
-    <section className="flex w-full items-center justify-center py-4">
-      <div ref={ref}>
-        <LoaderCircle className="animate-spin" />
+    <section ref={ref} className="flex h-max w-full flex-col rounded-md border">
+      <div className="bg-muted aspect-video w-full animate-pulse rounded-md"></div>
+      <div className="flex flex-col gap-1.5 px-2 py-2">
+        <div className="bg-muted h-6 w-3/4 animate-pulse rounded-md"></div>
+        <div className="bg-muted h-4 w-1/2 animate-pulse rounded-md"></div>
       </div>
     </section>
   );

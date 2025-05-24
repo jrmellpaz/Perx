70476705856b36acc3fb7aef1e4e026add4521c4
Jermel Lapaz <149228096+jrmellpaz@users.noreply.github.com>
2025-05-24 23:02:59 +0800

    [Fix][Jermel] Added auth checks in view page.

--- src/app/(consumer)/(private)/merchant-profile/[merchantId]/@tabs/archive/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/merchant/profile/@tabs/archive/page.tsx
-export default function ArchiveTab() {
-  return (
-    <div>
-      <h2>Archive</h2>
-      {/* Render archive content */}
-    </div>
-  );
-}
--- src/app/merchant/(private)/profile/@tabs/collections/page.tsx
+++ src/app/merchant/(private)/profile/@tabs/collections/page.tsx
@@ -1,9 +1,15 @@
-// app/merchant/profile/@tabs/collections/page.tsx
+import { Blocks } from 'lucide-react';
+
 export default function CollectionsTab() {
   return (
-    <div>
-      <h2>Collections</h2>
-      {/* Render collections content */}
+    <div className="flex size-full flex-col items-center-safe justify-center-safe gap-4 p-12">
+      <Blocks size={120} className="text-neutral-400" />
+      <div className="flex flex-col items-center gap-1">
+        <h1 className="font-mono text-2xl font-bold">Coming soon</h1>
+        <p className="text-mono text-sm text-neutral-500">
+          We're working hard to bring you this feature. Stay tuned!
+        </p>
+      </div>
     </div>
   );
 }
